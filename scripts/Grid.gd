7174a11c3c4f409dfa725daac16e88ae17603d4c
CRN91 <ak824@exeter.ac.uk>
2024-07-15 15:16:44 +0100

    Converted piece movement from oddr to axial and refactored to implement the hexgrid submodule

--- scripts/Grid.gd
+++ scripts/Grid.gd
@@ -1,48 +1,18 @@
 extends TileMap
 
+const HEXGRID = preload("res://Hexgrid/hex.gd")
+var HEX = HEXGRID.new()
+
 var Grid = {}
 var highlights = []
 @onready var piece = $"../Piece"
 
-func axial_to_oddr(hex):
-	var col = hex.x + (hex.y - (hex.y&1)) / 2
-	var row = hex.y
-	return Vector2i(col, row)
-
-var cube_direction_vectors = [
-	Vector3i(+1, 0, -1), Vector3i(+1, -1, 0), Vector3i(0, -1, +1), 
-	Vector3i(-1, 0, +1), Vector3i(-1, +1, 0), Vector3i(0, +1, -1), 
-]
-
-func cube_distance(a, b):
-	var vec = a - b
-	return max(abs(vec.x), abs(vec.y), abs(vec.z)) 
-
-func cube_neighbor(cube, direction):
-	return cube + cube_direction_vectors[direction]
-
-func cube_ring(center, radius):
-	var results = []
-	var hex = center + cube_direction_vectors[4] * radius
-
-	for i in range(6):
-		for j in range(radius):
-			results.append(hex)
-			hex = cube_neighbor(hex, i)
-	return results
-
-func cube_spiral(center, radius):
-	var results = [center]
-	for k in range(1,radius):
-		results = results + cube_ring(center, k)
-	return results
-
 func make_grid_axial():
 	# 7 wide 
-	var grid_list = cube_spiral(Vector3i(0,0,0), 4)
+	var grid_list = HEX.cube_spiral(Vector3i(0,0,0), 4)
 	var offset_i
 	for i in grid_list:
-		offset_i = axial_to_oddr(i)
+		offset_i = HEX.axial_to_oddr(i)
 		Grid[offset_i] = {
 			"Cube" : i
 		}
--- scripts/Piece.gd
+++ scripts/Piece.gd
@@ -1,8 +1,12 @@
 extends Node2D
+
+const HEXGRID = preload("res://Hexgrid/hex.gd")
+var HEX = HEXGRID.new()
+
 @onready var grid = $"../Grid"
 
 # Global coordinates of piece
-var pos = Vector2i(0,0)
+var pos_v = Vector2i(0,0)
 var supplies = 100
 
 # Checks the coords given relate to a valid hex
@@ -12,71 +16,28 @@ func valid_coords(coords):
 	else:
 		false
 	
-# Moves piece to grid coordinates
 func move_to(new_cell):
-	if valid_coords(new_cell):
-		pos = new_cell
-		var tiles = grid.get_used_cells_by_id(0, 0, Vector2i(0, 0))
-		# Size of tiles
-		var tile_size = grid.tile_set.tile_size
-		# Top cell, position calculated relative to here (origin)
-		var start_cell = Vector2i(3,0)
-		var world_position = (start_cell * tile_size) + (tile_size / 2)
-		# Coordinate difference between origin
-		var cell_difference = new_cell - start_cell
-		
-		var y_offset = cell_difference.y * 0.75 * tile_size.y
-		var x_offset
-		if cell_difference.y % 2:
-			x_offset = (tile_size.x * cell_difference.x) + (tile_size.x / 2)
-		else:
-			x_offset = tile_size.x * cell_difference.x
-
-		# This line sets the position of the game object in the scene
-		position = Vector2(world_position.x + x_offset, world_position.y + y_offset)
+	pos_v = new_cell
+	position = grid.map_to_local(HEX.axial_to_oddr(Vector2i(new_cell.x,new_cell.y)))
 
 func adjacent_move(direction):
-	if direction == 'w':
-		move_to(Vector2i(pos.x-1,pos.y))
-	elif direction == 'e':
-		move_to(Vector2i(pos.x+1,pos.y))
-	elif direction == 'ne':
-		if pos.y % 2:
-			move_to(Vector2i(pos.x+1, pos.y-1))
-		else:
-			move_to(Vector2i(pos.x, pos.y-1))
-	elif direction == 'nw':
-		if pos.y % 2:
-			move_to(Vector2i(pos.x, pos.y-1))
-		else:
-			move_to(Vector2i(pos.x-1, pos.y-1))
-	elif direction == 'se':
-		if pos.y % 2:
-			move_to(Vector2i(pos.x+1, pos.y+1))
-		else:
-			move_to(Vector2i(pos.x, pos.y+1))
-	elif direction == 'sw':
-		if pos.y % 2:
-			move_to(Vector2i(pos.x, pos.y+1))
-		else:
-			move_to(Vector2i(pos.x-1, pos.y+1))
+	move_to(HEX.cube_neighbor(HEX.axial_to_cube(pos_v), direction))
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	move_to(pos)
-	
+	move_to(pos_v)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ne"):
-		adjacent_move("ne")
-	if Input.is_action_just_pressed("nw"):
-		adjacent_move("nw")
 	if Input.is_action_just_pressed("e"):
-		adjacent_move("e")
-	if Input.is_action_just_pressed("w"):
-		adjacent_move("w")
-	if Input.is_action_just_pressed("sw"):
-		adjacent_move("sw")
-	if Input.is_action_just_pressed("se"):
-		adjacent_move("se")
+		adjacent_move(0)
+	elif Input.is_action_just_pressed("ne"):
+		adjacent_move(1)
+	elif Input.is_action_just_pressed("nw"):
+		adjacent_move(2)
+	elif Input.is_action_just_pressed("w"):
+		adjacent_move(3)
+	elif Input.is_action_just_pressed("sw"):
+		adjacent_move(4)
+	elif Input.is_action_just_pressed("se"):
+		adjacent_move(5)
